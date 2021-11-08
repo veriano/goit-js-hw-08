fcc74ead170db0955bee4d724040e772a3fa237a
veriano <veriano@ukr.net>
2021-11-08 17:48:37 +0200

    in processing #2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,18 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const creatGallery = (galleryItems) =>
+    galleryItems.map(({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+</a>`).join('');
+galleryContainer.insertAdjacentHTML('afterbegin', creatGallery(galleryItems));
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+const iframe = document.getElementById('iframe');
+const player = new Vimeo.Player(iframe);
 
+    player.on('timeupdate', function(data) {
+        let fire = data.seconds;
+        localStorage.setItem('videoplayer-current-time', `${fire}`);
+    });
+
+
+    player.setCurrentTime().then(function(seconds) {
+        seconds = fire;
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            if(seconds < 0 || seconds > data.duration) {
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
